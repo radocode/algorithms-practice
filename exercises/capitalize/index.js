a805d1ce2796a37833233b5e68f9c3898656ea85
Francisco González <franciscogonzalez@MacBook-Pro.local>
2020-07-22 18:15:37 -0400

    avances

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,30 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // mi solucion
+  // return str.split(' ').map(char => char.split('')[0].toUpperCase().concat(char.substring(1,char.length))).join(' ');
+  // return str.split(' ').map(char => char.split('')[0].toUpperCase().concat(char.slice(1))).join(' ');
+
+  // solution 1
+  // const words = [];
+  // for (let word of str.split(' ')) {
+  //   words.push(word[0].toUpperCase()+word.slice(1));
+  // }
+  // return words.join(' ');
+
+  // solution 2
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i-1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+
+}
 
 module.exports = capitalize;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// solution 1, iterative. always be sure to make pseudocode first!
+// function steps(n) {
+  // for (let row = 0; row < n; row++) {
+  //   let stair = '';
+  //   for (let column = 0; column < n; column++) {
+  //     if(column <= row){
+  //       stair += '#';
+  //     } else {
+  //       stair += ' ';
+  //     }
+  //   }
+  //   console.log(stair);
+  // }
+
+  // }
+
+// solution 2, recursion
+function steps(n, row = 0, stair='') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row+1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+}
 
 module.exports = steps;
